976848f7af4fe3dfb38d0df0772c8b15047385d2
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2024-01-29 05:11:12 -0500

    changed main image for main home added name cleared the nav bar

--- src/script/Home.js
+++ src/script/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
-import '../stylesheet/Navigator.css'
+import '../stylesheet/Home.css'
 function Home() {
 
     return (
         <React.Fragment>
             <div className={'home'}>
                 {/*출처 링크 : https://gongu.copyright.or.kr/gongu/wrt/wrt/view.do?wrtSn=13215201&menuNo=200018*/}
-                <img className={'vincent-van-gogh-stary-night main-home-image'} id="wrtImg"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
-                    alt={'Vincent Willem van Gogh_The Starry Night'} />
+                <img className={'main-home-image'} id="skyline-to-korea"
+                    src="/img/Skyline-to-Korea-2023.jpeg"
+                    alt={'Skyline-to-Korea'} />
+                <h1 className={'name'} align="right">Yoonje <br/> Lee</h1>
+
             </div>
         </React.Fragment>
     );
--- src/script/Navigator.js
+++ src/script/Navigator.js
@@ -7,25 +7,30 @@ function Navigator() {
     return (
 
         <React.Fragment>
-            <div className="MenuBar">
-                <Link className="Menu" to="/"> Home </Link>
-                <Link className="Menu" to="/"> about </Link>
-                <Link className="Menu" to="/"> experiences </Link>
-                <Link className="Menu" to="/"> portfolio </Link>
-                <Link className="Menu" to="/"> contact </Link>
-                <Link className="Menu"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    to="https://www.linkedin.com/in/yoonje-lee-0573a4214/"
-                > linkedin </Link>
-                <Link className="Menu"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    to="https://github.com/yoonlee201"
-                >
-                    <img className="logo" src="/github-mark-white.svg" alt="github logo" />
-                    {/*<object data="../image/github-mark-white.svg"> </object>*/}
-                </Link>
+            <div className="NavBar">
+                <Link className="Menu link-to-home" to="/">YL</Link>
+                <div className="MenuBar">
+                    <Link className="Menu" to="/">about</Link>
+                    <Link className="Menu" to="/">experiences</Link>
+                    <Link className="Menu" to="/">portfolio</Link>
+                    <Link className="Menu" to="/">contact</Link>
+                    <Link className="Menu menu-logo"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        to="https://www.linkedin.com/in/yoonje-lee-0573a4214/"
+                    > <img className="logo linkedin"
+                           src="/img/LI-In-Bug.png"
+                           alt="Linkin logo" />
+                    </Link>
+                    <Link className="Menu menu-logo"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        to="https://github.com/yoonlee201"
+                    > <img className="logo"
+                           src="/img/github-mark-white.svg"
+                           alt="github logo" />
+                    </Link>
+                </div>
             </div>
         </React.Fragment>
     );
