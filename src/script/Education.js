6be71bcfb54467967acc21eb1e842ff52416549e
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2024-02-13 20:20:16 -0500

    removed Timeline.js and changed separated to Experiences.js and Education.js

---
+++ src/script/Education.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../stylesheet/Experiences.css'
+
+function Education() {
+
+    return (
+        <React.Fragment>
+            <div className="education" id="education">
+                <h1 className={'section-title'}>Education</h1>
+                <div className="vertical-container">
+                    <VerticalTimeline
+                        layout={'1-column-left'}>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="2022 - present"
+                            iconStyle={{ background: '#fff', color: '#fff' }}
+                            icon={<img src={"img/Vertical_VT_Full_Color_RGB.svg"} alt="myLogo" />}
+                        >
+                            <h3 className="vertical-timeline-element-title">Virginia Tech</h3>
+                            <h4 className="vertical-timeline-element-subtitle">B.S. Computer Science & HCI Minor</h4>
+                            {/*<p>*/}
+                            <ul>
+                                <li>GPA: 3.98 out of 4.0</li>
+                                <li>President’s List (Fall 2022)</li>
+                                <li>Dean’s List (Spring 2023 & Fall 2023)</li>
+                                <li>Expected Graduation Date: 2025</li>
+                            </ul>
+                            {/*</p>*/}
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="2021 - 2022"
+                            iconStyle={{ background: '#fff', color: '#fff' }}
+                            icon={<img src={"img/Vertical_VT_Full_Color_RGB.svg"} alt="myLogo" />}
+                        >
+                            <h3 className="vertical-timeline-element-title">George Mason University</h3>
+                            <h4 className="vertical-timeline-element-subtitle">B.S. Computer Science</h4>
+                            {/*<p>*/}
+                            <ul>
+                                <li>GPA: 3.97 out of 4.0</li>
+                                <li>Dean’s List (Fall 2021 & Spring 2022)</li>
+                            </ul>
+                            {/*</p>*/}
+                        </VerticalTimelineElement>
+                    </VerticalTimeline>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+
+export default Education;
---
+++ src/script/Experiences.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../stylesheet/Experiences.css'
+
+function Experiences() {
+
+    return (
+        <React.Fragment>
+            <div className="experiences" id="experiences">
+                <h1 className={'section-title'}>Experiences</h1>
+                <div className="vertical-container">
+                    <VerticalTimeline
+                        layout={'1-column-left'}>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2024 May"
+                            iconStyle={{ background: '#fff' , color: '#fff' }}
+                        >
+                            <h3 className="vertical-timeline-element-title">Software Engineer Intern@Versy</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Remote</h4>
+                            <p>
+                                Internship starts at May 2024
+                            </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            date="2023 - 2024"
+                            iconStyle={{ background: '#fff', color: '#fff' }}
+                        icon={<img src={"img/Vertical_VT_Full_Color_RGB.svg"} alt="myLogo" />}
+                        >
+                            <h3 className="vertical-timeline-element-title">Undergraduate Teaching Assistant@Virginia Tech </h3>
+                            <h4 className="vertical-timeline-element-subtitle">Blacksburg, VA</h4>
+                            <ul>
+                                <li>Communicated effectively with faculty regarding 500+ student progress and participated in professional development opportunities</li>
+                                <li>Created documentation of instructions for students to set up ssh credentials in VS Code</li>
+                                <li>Assisted students with course homework, assignments, and project</li>
+                                <li>Introduction to Computer Organization I, 9 hours per week</li>
+
+                            </ul>
+                        </VerticalTimelineElement>
+                    </VerticalTimeline>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+
+export default Experiences;
